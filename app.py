6746c781249040c651ffe6cd6e58d39fba113bd2
Sadid369 <sadid.jones@gmail.com>
2022-05-07 23:43:06 +0600

    5/7/2022

--- app.py
+++ app.py
@@ -1,28 +1,28 @@
-class Point:
-    deafult_color = "red"
+# class Point:
+#     deafult_color = "red"
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
 
-    @classmethod
-    def zero(cls):
-        return cls(0, 0)
+#     @classmethod
+#     def zero(cls):
+#         return cls(0, 0)
 
-    def __str__(self):
-        return f" ({self.x} , {self.y})"
+#     def __str__(self):
+#         return f" ({self.x} , {self.y})"
 
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
+#     def __eq__(self, other):
+#         return self.x == other.x and self.y == other.y
 
-    def __gt__(self, other):
-        return self.x > other.x and self.y > other.y
+#     def __gt__(self, other):
+#         return self.x > other.x and self.y > other.y
 
-    def __add__(self, other):
-        return Point(self.x + other.x, self.y + other.y)
+#     def __add__(self, other):
+#         return Point(self.x + other.x, self.y + other.y)
 
-    def draw(self):
-        print(f"Draw ({self.x} , {self.y})")
+#     def draw(self):
+#         print(f"Draw ({self.x} , {self.y})")
 
 
 # Point.deafult_color = "Yellow"
@@ -34,8 +34,48 @@ class Point:
 # print(another.deafult_color)
 # another.draw()
 
-point = Point(1, 2)
-other = Point(2, 3)
-print(point + other)
+# point = Point(1, 2)
+# other = Point(2, 3)
+# print(point + other)
 
-point.draw()
+# point.draw()
+
+# class Product:
+#     def __init__(self, price):
+#         self.price = price
+
+#     @property
+#     def price(self):
+#         return self.__price
+
+#     @price.setter
+#     def price(self, value):
+#         if value < 0:
+#             raise ValueError("Price cannot be negative.")
+#         self.__price = value
+
+
+# product = Product(-10)
+
+# print(product.price)
+class Animal:
+    def __init__(self):
+        self.age = 1
+
+    def eat(self):
+        print("eat")
+
+
+class Mammal(Animal):
+    def walk(self):
+        print("walk")
+
+
+class Fish(Animal):
+    def swim(self):
+        print("swim")
+
+
+m = Mammal()
+
+m.eat()
