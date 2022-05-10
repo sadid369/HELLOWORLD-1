cc20a9576424cd18e438afce49019a6198a52e15
Sadid369 <sadid.jones@gmail.com>
2022-05-10 22:08:52 +0600

    10/05/2022

--- app.py
+++ app.py
@@ -127,32 +127,5 @@
 
 
 # stream = MemoryStream()
-
-from abc import ABC, abstractmethod
-
-
-class UIControl(ABC):
-    @abstractmethod
-    def draw(self):
-        pass
-
-
-class TextBox(UIControl):
-    def draw(self):
-        print("TextBox")
-
-
-class DropDownList(UIControl):
-    def draw(self):
-        print("DropDownList")
-
-
-def draw(controls):
-    for control in controls:
-        control.draw()
-
-
-ddl = DropDownList()
-textbox = TextBox()
-print(isinstance(ddl, UIControl))
-draw([ddl, textbox])
+from ecommerce.shopping import sales
+sales.calc_tax()
